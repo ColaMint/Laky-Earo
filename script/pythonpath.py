27c8a9fe4c73b0ab01b3211aaef7b04c8e8ae0c6
xiaominghupan <463785757@qq.com>
2016-03-21 11:59:51 +0800

    pythonpath script

--- script/pythonpath.py
+++ script/pythonpath.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+import os
+
+
+def find_parent(path, times=1):
+    parent = os.path.dirname(path)
+    return parent if times <= 1 else find_parent(parent, times-1)
+
 if __name__ == '__main__':
-    pass
+    project_dir = find_parent(os.path.abspath(__file__), 2)
+    print 'export PYTHONPATH=$PYTHON_PATH:%s' % (project_dir,)
